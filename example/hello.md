1755198ea49917ac3a034a78a46f2404e3b1b973
Kaleo <kaleocheng@gmail.com>
2019-04-22 14:42:12 +0800

    Add initial version

---
+++ example/hello.md
@@ -0,0 +1,9 @@
+# Hello
+
+this is a example.
+
+```sequence
+a -> b: hello
+```
+
+
---
+++ index.js
@@ -0,0 +1,71 @@
+const pkg = require('./package.json')
+const commander = require('commander')
+const fs = require('fs')
+const mdRender = require('markdowner-pretty/render')
+const swg2md = require('swg2md')
+
+
+
+function read(f) {
+    if (!fs.existsSync(f)) {
+        return
+    }
+    return fs.readFileSync(f, 'utf8')
+}
+
+function renderToMD(contents) {
+    const output = []
+    contents.forEach((vaule) => {
+        output.push(mdRender(vaule, { path: commander.buildDir }))
+    })
+    for (let c of output) {
+        console.log(c)
+    }
+
+}
+
+function exit(info) {
+    console.log(info)
+    process.exit(0)
+}
+commander
+    .version(pkg.version)
+    .option('--build-dir [buildDir]', 'The assets output path')
+    .option('--swagger', 'Parse swagger file')
+    .parse(process.argv)
+
+
+async function main() {
+    const output = []
+    if (commander.args.length < 1) {
+        exit('Need at last one swagger or markdown file')
+    }
+
+    const tasks = []
+    let contents = []
+    for (f of commander.args) {
+        let content = read(f)
+        if (!content) {
+            console.log(`${f} is not exist`)
+            break
+        }
+
+        if (commander.swagger) {
+            content = await swg2md.render(f)
+            tasks.push(content)
+
+        } else {
+            contents.push(content)
+        }
+    }
+
+    if (commander.swagger) {
+        Promise.all(tasks).then(renderToMD)
+    } else {
+        renderToMD(contents)
+    }
+}
+
+
+
+main();
